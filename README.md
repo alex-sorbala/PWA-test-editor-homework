0a947bfe75f4c5b8ab3a37e79062822253e91db4
Alexandru S <alexandru11723@gmail.com>
2023-04-09 23:37:35 -0600

    initial code

---
+++ README.md
@@ -0,0 +1,37 @@
+# Text-Editor-PWA
+
+![GitHub License](https://img.shields.io/badge/License-MIT-green)
+
+## Description
+
+
+
+Link to deployed app: 
+
+## Table of Contents
+
+- [Installation](#installation)
+
+- [Usage](#usage)
+
+- [License](#license)
+
+- [Contributing](#contributing)
+
+- [Questions](#Questions)
+
+## Installation
+
+The application may be installed as a PWA using the install button on the page or clicking the install icon on the right side of the browser's search bar.
+
+## Usage
+
+The application can be installed as a PWA using the install button on the page or clicking the install icon on the right side of the browser's search bar. 
+
+## License
+
+MIT
+
+## Contributing
+
+N/A
---
+++ client/src/js/install.js
@@ -0,0 +1,32 @@
+const butInstall = document.getElementById("buttonInstall");
+
+// Logic for installing the PWA
+window.addEventListener("beforeinstallprompt", (event) => {
+  
+  // Store the triggered events
+  window.deferredPrompt = event;
+
+  // Show install button
+  butInstall.classList.toggle("hidden", false);
+});
+
+
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+
+  // Show prompt
+  promptEvent.prompt();
+
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle("hidden", true);
+});
+
+window.addEventListener("appinstalled", (event) => {
+  window.deferredPrompt = null;
+});
